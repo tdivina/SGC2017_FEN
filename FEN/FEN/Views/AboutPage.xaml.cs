8927afa7780c8441e57dafa357e49b33c5e8592e
James Montemagno <james.montemagno@gmail.com>
2017-10-02 15:14:10 +0200

    Add initial code for iOS/Android

---
+++ FEN/FEN/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace FEN
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
